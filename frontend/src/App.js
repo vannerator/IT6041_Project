902309bce4f3bf50374b30714785f9447b39b7e7
James Vanner <james.vanner@outlook.com>
2022-08-29 08:43:17 +1200

    added frontend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Kia ora World!</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
