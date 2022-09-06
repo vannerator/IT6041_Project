e670d7660f9be69fe0e620f219a55dc5593ee810
James Vanner <james.vanner@outlook.com>
2022-09-06 14:42:45 +1200

    home page setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <h1>Home</h1>
+    </>
+  );
 }
 
-export default Home
+export default Home;
