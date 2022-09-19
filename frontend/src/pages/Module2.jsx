6ceb85e6a6f28ba1bff2f1efc77da315c9a00ee6
Alia027 <alia.rapana@gmail.com>
2022-09-20 00:57:49 +1200

    module updates

--- frontend/src/pages/Module2.jsx
+++ frontend/src/pages/Module2.jsx
@@ -17,37 +17,130 @@ function Module2() {
   return (
     <>
       <div className="page-content">
-        <h1>Rua - Loops and Functions</h1>
-        <div className="heading">
-          <h2>Basic Syntax </h2>
+        <h1>Rua - Python Syntax </h1>
+        <h2>Basic Syntax </h2>
+        <p>In this lesson, you will learn about python syntax.</p>
 
-<h4>In this lesson, you will learn about python syntax.</h4>
+        <br/> <br/>
 
-<p>The syntax is the set of rules that define the structure of the Python language.
-  <br/> It represents the rules or structure that control the layout of the keywords,
-  <br/> symbols, and punctuation of Python programming. </p>
+        <container maxWidth = "lg"
+          style = {{borderStyle: "solid", borderColor: "black", height: "100%", 
+          width: "90%"}}>
 
-<h3>Python syntax can be executed by writing directly in the Command Line: </h3>
-<p>print(”Kia Ora, World!") 
-<br/>Kia Ora, World! </p>
+          <br/> 
 
-<p>Or by creating a python file on the server, using the .py file extension, 
+          <h3> Syntax </h3>
+
+        <p>The syntax is the set of rules that define the structure of the Python language.
+          <br/> It represents the rules or structure that control the layout of the keywords,
+          <br/> symbols, and punctuation of Python programming. </p>
+
+          <br/>
+
+        <h4>Python syntax can be executed by writing directly in the Command Line: </h4>
+        <h4>print(”Kia Ora, World!") 
+          <br/>Kia Ora, World! </h4>
+
+          <br/>
+
+        <p>Or by creating a python file on the server, using the .py file extension, 
   <br/>and running it in the Command Line:
-  <br/>C:\Users\Your Name - python myfile.py </p>
- 
-<h3>Identifiers </h3> 
-<p>Python uses identifiers that identify variables, functions, modules, classes, and other 
-<br/>objects in Python. They are case sensitive. Apples and apples are examples of two different identifiers. </p>
+          <br/>C:\Users\Your Name - python myfile.py </p>
 
-<p>The name of an identifier needs to be a letter or underscore (_). The following characters can be
-<br/> alphanumeric or underscore.</p>
+          <br/> <br/>
 
-<p>In addition, you cannot use Python keywords for naming identifiers. </p>
+          </container>
 
-<h3>Keywords</h3>
+          <br/> <br/> <br/>
 
+          <container maxWidth = "lg"
+        style = {{borderStyle: "solid", borderColor: "black", height: "100%", 
+        width: "90%"}}>
 
-        </div>
+          <br/>
+
+        <h3>Identifiers </h3> 
+        
+        <p>Python uses identifiers that identify variables, functions, modules, classes, and other 
+          <br/>objects in Python. They are case sensitive. Apples and apples are examples of two different identifiers. </p>
+
+          <br/>
+
+        <p>The name of an identifier needs to be a letter or underscore (_). The following characters can be
+          <br/> alphanumeric or underscore.</p>
+
+          <br/>
+
+        <p>In addition, you cannot use Python keywords for naming identifiers. </p>
+
+          <br/>
+
+        <h3>Keywords</h3>
+
+        <p>Keywords have special meanings in Python. </p>
+          <br/> <br/>
+          
+          <h4>The following are keywords: </h4>
+        
+        <table>
+        <tr>
+          <td>False</td>
+          <td>class</td>
+          <td>finally</td>
+          <td>from</td>
+        </tr>
+        <tr>
+          <td>is</td>
+          <td>return</td>
+          <td>None</td>
+          <td>nonlocal</td>
+        </tr>
+        <tr>
+          <td>continue</td>
+          <td>for</td>
+          <td>lambda</td>
+          <td>while</td>
+        </tr>
+        <tr>
+          <td>and</td>
+          <td>del</td>
+          <td>global</td>
+          <td>not</td>
+        </tr>
+        <tr>
+          <td>with</td>
+          <td>as</td>
+          <td>elif</td>
+          <td>if</td>
+        </tr>
+        <tr>
+          <td>or</td>
+          <td>yield</td>
+          <td>assert</td>
+          <td>else</td>
+        </tr>
+        <tr>
+          <td>import</td>
+          <td>pass</td>
+          <td>break</td>
+          <td>except</td>
+        </tr>
+      </table>
+
+        <br/> <br/>
+
+        </container>
+
+        <br/> <br/>
+
+        <h1> Test your knowledge and take the quiz</h1>
+
+        <br/>
+
+        <button className="btn btn-big">Quiz</button>
+
+        <br/>
+
       </div>
     </>
   );
