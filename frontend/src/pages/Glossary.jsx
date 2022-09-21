5bcabe44a7c5fa761fce33e3bbcdc52c9bc99822
James Vanner <james.vanner@outlook.com>
2022-09-22 08:49:53 +1200

    .

--- frontend/src/pages/Glossary.jsx
+++ frontend/src/pages/Glossary.jsx
@@ -6,54 +6,48 @@ function Glossary() {
     <>
       <div className="page-content">
         <div className="heading">
-            <h1> Glossary </h1>
-          <p>
-            Maori terms translated
-          </p>
+          <h1> Glossary </h1>
+          <p>Maori terms translated</p>
         </div>
-
         <table>
-            <tr>
-              <th>Term</th>
-              <th>Description</th>
-            </tr>
-            <tr>
-              <td>Tikanga Waehere</td>
-              <td>The coding method</td>
-            </tr>
-            <tr>
-              <td>Tahi</td>
-              <td>One</td>
-            </tr>
-            <tr>
-              <td>Rua</td>
-              <td>Two</td>
-            </tr>
-            <tr>
-              <td>Toru</td>
-              <td>Three</td>
-            </tr>
-            <tr>
-              <td>Kia Ora</td>
-              <td>Hello</td>
-            </tr>
-            <tr>
-              <td>Nau Mai, Haere Mai</td>
-              <td>Welcome, come</td>
-            </tr>            <tr>
-              <td>Ka Pai</td>
-              <td>It will be good</td>
-            </tr>         
-          </table>
-
-          <br/> <br/> <br/>
-
+          <tr>
+            <th>Term</th>
+            <th>Description</th>
+          </tr>
+          <tr>
+            <td>Tikanga Waehere</td>
+            <td>The coding method</td>
+          </tr>
+          <tr>
+            <td>Tahi</td>
+            <td>One</td>
+          </tr>
+          <tr>
+            <td>Rua</td>
+            <td>Two</td>
+          </tr>
+          <tr>
+            <td>Toru</td>
+            <td>Three</td>
+          </tr>
+          <tr>
+            <td>Kia Ora</td>
+            <td>Hello</td>
+          </tr>
+          <tr>
+            <td>Nau Mai, Haere Mai</td>
+            <td>Welcome, come</td>
+          </tr>{" "}
+          <tr>
+            <td>Ka Pai</td>
+            <td>It will be good</td>
+          </tr>
+        </table>
+        <br /> <br /> <br />
         <Link to="/modules">
-          <button className="btn btn-big"> Modules </button>
+          <button className="btn btn-big"> Continue </button>
         </Link>
-
-        <br/> <br/> <br/>
-
+        <br /> <br /> <br />
       </div>
     </>
   );
